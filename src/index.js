4a252290dd6e20942c79b0e1d05d29d93e8acec2
Mizodeus <151405871+Mizodeus@users.noreply.github.com.>
2026-09-09 15:07:00 +0500

    Add style

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import "./styles.css";
 import { createProject, createTodo } from "./app.js";
+
+const defaultProject = createProject("Default");
+
+const todo = createTodo(
+  defaultProject,
+  "Learn JavaScript",
+  "Study modules",
+  "2026-09-10",
+  "high",
+);
+
+console.log(defaultProject);
